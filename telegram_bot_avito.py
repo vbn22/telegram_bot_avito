bc87517bd493f53140562322341689a2d8d9848f
Michael <root@dev.vaneev.muzis.ru>
2019-02-01 10:31:39 +0300

    check_walk_distance

--- telegram_bot_avito.py
+++ telegram_bot_avito.py
@@ -13,6 +13,7 @@ from models import User
 import json
 import asyncio
 import hashlib
+import re
 
 BOT_TOKEN = '*'
 
@@ -26,7 +27,7 @@ bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(bot)
 
 HELP = '''
-/add LINK  - добавить ссылку с авито со СПИСОКМ ОБЬЯВЛЕНИЙ
+/add base_link  - добавить ссылку с авито со СПИСОКМ ОБЬЯВЛЕНИЙ
 /all  - показатьвсе что добавлено
 /clear - удалить все ссылки
 /help - помощь
@@ -65,7 +66,7 @@ async def add_link(message: types.Message):
     user = User.get(User.chat_id == message.chat.id)
     links = user.get_links()
     text = message.text.replace('/add ','')
-    if text.startswith('http') and ('avito' in text or 'youla.ru' in text):
+    if text.startswith('http') and ('avito' in text or 'youla.ru' in text or 'cian.ru' in text):
         links.append(text)
         user.links = json.dumps(links)
         user.save()
@@ -114,6 +115,39 @@ def avito_handler(url):
         res.append(ad_link)
     return res
 
+async def check_walk_distance(url):
+    max_distance_km = 2
+    def get_distance(el):
+        el=el.replace(u'\xa0',u'|')
+        try:
+            res=re.findall(r'\((.*?)\)',el)[0].split('|')
+        except IndexError:
+            return False
+        return (res[1] == 'м' and float(res[0]) < max_distance_km * 1000) or (res[1] == 'км' and float(res[0]) < max_distance_km)
+    ad_page = BeautifulSoup(requests.get(url, verify=True).text)
+    items_metro = ad_page.findAll('span',{'class':'item-map-metro'})
+    distances = [x for x in items_metro if get_distance(x.text)]
+    await asyncio.sleep(20)
+    return any(distances)
+
+
+def cian_handler(url):
+    def find(elements,tag,name):
+        return [x for x in elements.findAll(tag) if name in ''.join(x.attrs.get('class',''))]
+
+    res = []
+    soup = BeautifulSoup(requests.get(url, verify=True).text)
+    wrappers = find(soup,'div','wrapper')
+    most_big = sorted([(i,len(f.text))for i,f in enumerate(wrappers)],key=lambda x:x[1],reverse=True)[0][0]
+
+    for el in find(wrappers[most_big],'div','card'):
+        try:
+            ad_link = find(el,'a','header')[0].attrs['href']
+        except IndexError:
+            continue
+        res.append(ad_link)
+    return res
+
 def youla_handler(url):
     res = []
     format_name = lambda x: 'https://youla.ru/' + x['href']
@@ -125,31 +159,41 @@ def youla_handler(url):
         except IndexError:
             continue
     return res
-    
+
+
+def url_handler(url):
+    if 'avito' in url:
+        return avito_handler(url)
+    elif 'youla.ru' in url:
+        return youla_handler(url)
+    elif 'cian.ru' in url:
+        return cian_handler(url)
+    return []
+
 async def main():
+    print('start')
     while True:
-        await asyncio.sleep(60*5)
         for u in User.select():
             messages_to_send = []
             old_ads = u.get_ads()
-            def url_handler(url):
-                urls = []
-                if 'avito' in url:
-                    urls = avito_handler(url)
-                elif 'youla.ru' in url:
-                    urls = youla_handler(url)
-                for ad_link in urls:
+            for base_link in u.get_links():
+                for ad_link in url_handler(base_link):
                     if len(messages_to_send) > 3:
                         break
-                    if 'redirect' in ad_link or md5_from_string(ad_link) in old_ads:
+                    if 'redirect' in ad_link or (md5_from_string(ad_link) in old_ads):
                         continue
-                    messages_to_send.append(ad_link)
+                    if 'avito' in base_link and 'kvartiry' in ad_link:
+                        walk_distance = await check_walk_distance(ad_link)
+                        if walk_distance:
+                            messages_to_send.append(ad_link)
+                    else:
+                        messages_to_send.append(ad_link)
                     old_ads.append(md5_from_string(ad_link))
-            [url_handler(link) for link in u.get_links()]
             u.showed_ads = json.dumps(old_ads)
             u.save()
             if messages_to_send:
                await bot.send_message(u.chat_id, '\n'.join(messages_to_send))
+        await asyncio.sleep(60*5)
 
 
 if __name__ == '__main__':
